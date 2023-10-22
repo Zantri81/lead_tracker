3f4feb06b975575677c7cc19ccfa0b3b6b9cd5d2
Zantri81 <tristanzanetti@yahoo.fr>
2023-10-22 23:49:00 +0200

    first addEventListener

---
+++ index.js
@@ -0,0 +1,5 @@
+let inputBTN = document.getElementById("input-btn")
+
+inputBTN.addEventListener("click", function() {         // react to an event exemple : click on button 
+    console.log("Button clicked from addEventListener")
+})        
