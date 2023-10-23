de84d8fed665b91587b78ed57b5394d46dff601f
Zantri81 <tristanzanetti@yahoo.fr>
2023-10-23 15:58:20 +0200

    "improving"

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let inputBTN = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+
+const inputEL = document.getElementById("input-el")     //const can't be reassigned. If possible, use const. If not, use let.
+const inputBTN = document.getElementById("input-btn")
+const ulEL = document.getElementById("ul-el")
+
+
 
 inputBTN.addEventListener("click", function() {         // react to an event exemple : click on button 
-    console.log("Button clicked from addEventListener")
+    myLeads.push(inputEL.value)                 //add in an array. adding value will here, copy the text in the input
+    console.log(myLeads)
 })        
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEL.textContent += myLeads[i] + " "
+}
